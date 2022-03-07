be2f619d366eb50f77badcc287b4bd2a86d1e48c
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-07 20:45:36 +0300

    Added base CI config

--- project/main.c
+++ project/main.c
@@ -3,41 +3,6 @@
 
 #include "solver.h"
 
-/*
-typedef struct {
-  double x;
-  double y;
-} point_t;
-
-int find_quadratic_coefficients(const point_t *p1,
-				   const point_t *p2,
-				   const point_t *p3,
-				   double *coef_a,
-				   double *coef_b,
-				   double *coef_c) {
-  if (coef_a != NULL) {
-	*coef_a = p1->y / ((p1->x - p2->x) * (p1->x - p3->x)) +
-		      p2->y / ((p2->x - p1->x) * (p2->x - p3->x)) +
-			  p3->y / ((p3->x - p1->x) * (p3->x - p2->x));
-  }
-
-  if (coef_b != NULL) {
-	*coef_b = (p1->y * (p3->x + p2->x)) / ((p1->x - p2->x) * (p1->x - p3->x)) +
-		      (p2->y * (p1->x + p3->x)) / ((p2->x - p1->x) * (p2->x - p3->x)) +
-		      (p3->y * (p1->x + p2->x)) / ((p3->x - p1->x) * (p3->x - p2->x));
-	*coef_b *= -1.0;
-  }
-
-  if (coef_c != NULL) {
-	*coef_c = (p1->y * p2->x * p3->x) / ((p1->x - p2->x) * (p1->x - p3->x)) +
-		      (p2->y * p1->x * p3->x) / ((p2->x - p1->x) * (p2->x - p3->x)) +
-		      (p3->y * p1->x * p2->x) / ((p3->x - p1->x) * (p3->x - p2->x));
-  }
-
-  return 1;
-}
-*/
-
 int main() {
   point_t p1 = {0, 0.02};
   point_t p2 = {0.3, 0.4};
