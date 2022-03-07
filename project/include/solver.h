8c8aaaa078a37f61cdc05e3e55ad339d1ba1cb4c
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-07 21:26:56 +0300

    Trying to add automaticaly formating

--- project/include/solver.h
+++ project/include/solver.h
@@ -12,11 +12,8 @@ typedef enum {
   WRONG_INPUT
 } solver_status_t;
 
-int find_quadratic_coefficients(const point_t *p1,
-								const point_t *p2,
-								const point_t *p3,
-								double *coef_a,
-								double *coef_b,
-								double *coef_c);
+int find_quadratic_coefficients(const point_t *p1, const point_t *p2,
+                                const point_t *p3, double *coef_a,
+                                double *coef_b, double *coef_c);
 
-#endif //PARABOLA_SOLVER_H_
+#endif  // PARABOLA_SOLVER_H_
