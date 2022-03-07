7f179d38470fe81d75b91fb3c978c55b6390d9bf
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-07 23:11:57 +0300

    Modified code style checking

--- scripts/clang-formating.sh
+++ scripts/clang-formating.sh
@@ -3,15 +3,18 @@
 
 TMP_DIR="./tmp"
 LOG_FILE="./tmp/clang-formating.log"
+TMP_FORM_FILE="./tmp/formated-file"
 mkdir -p $TMP_DIR
 touch $LOG_FILE
+touch $TMP_FORM_FILE
 
-sudo apt install clang-format >> $LOG_FILE
+#sudo apt install clang-format >> $LOG_FILE
 
 STYLE="Google"
 FORMATING_FILES=("./project/main.c" "./project/src/solver.c" "./project/include/solver.h")
 for file in ${FORMATING_FILES[*]}
 do
     echo $file
-    clang-format -style=$STYLE -i $file >> $LOG_FILE || (echo "Error, check $LOG_FILE")
+    clang-format -style=$STYLE $file > $TMP_FORM_FILE || (echo "Error, check $LOG_FILE")
+    diff $file $TMP_FORM_FILE
 done
