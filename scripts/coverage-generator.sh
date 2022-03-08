0f89cfdbebb59a6ea9fecde5822ed65fc8d4a5be
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-08 13:38:09 +0300

    UPD coverage generator script

--- scripts/coverage-generator.sh
+++ scripts/coverage-generator.sh
@@ -9,7 +9,7 @@ pip install gcovr
 cd $BUILD_DIRECTORY || (echo "Error. $BUILD_DIRECTORY not created." && exit 1)
 ctest
 cd ..
-EXCLUDING_FILES=".*_deps.*|.*tests.*"
+EXCLUDING_FILES=".*_deps.*|.*tests.*|project/main.c"
 #EXCLUDING_FILES=".*_deps.*"
 REPORT_FILE_NAME="coverage.xml"
 gcovr -r . ./build -e $EXCLUDING_FILES -o $REPORT_FILE_NAME --xml
