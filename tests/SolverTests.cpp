ef0bbf619ad7d51e698b91e11c8eff0aa313b889
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-08 13:43:00 +0300

    Fixed linters error in SolverTests.cpp

--- tests/SolverTests.cpp
+++ tests/SolverTests.cpp
@@ -20,7 +20,7 @@ struct SolverTestsSolution : public testing::Test {
 };
 
 static double quadratic_function(double coef_a, double coef_b, double coef_c,
-                                 double x) {
+								 double x) {
   return coef_a * x * x + coef_b * x + coef_c;
 }
 
@@ -31,7 +31,7 @@ TEST_F(SolverTestsSolution, BasicSolution) {
   double expected_c = 1.0 / 50.0;
 
   int status =
-      find_quadratic_coefficients(&p1, &p2, &p3, &sol_a, &sol_b, &sol_c);
+	  find_quadratic_coefficients(&p1, &p2, &p3, &sol_a, &sol_b, &sol_c);
   EXPECT_EQ(status, SOLUTION_FOUND);
   ASSERT_DOUBLE_EQ(expected_a, sol_a);
   ASSERT_DOUBLE_EQ(expected_b, sol_b);
@@ -40,25 +40,25 @@ TEST_F(SolverTestsSolution, BasicSolution) {
 
 TEST_F(SolverTestsSolution, InterpolationProperties) {
   int status =
-      find_quadratic_coefficients(&p1, &p2, &p3, &sol_a, &sol_b, &sol_c);
+	  find_quadratic_coefficients(&p1, &p2, &p3, &sol_a, &sol_b, &sol_c);
   EXPECT_EQ(status, SOLUTION_FOUND);
   ASSERT_NEAR(quadratic_function(sol_a, sol_b, sol_c, p1.x), p1.y,
-              ABS_SOL_ERROR);
+			  ABS_SOL_ERROR);
   ASSERT_NEAR(quadratic_function(sol_a, sol_b, sol_c, p2.x), p2.y,
-              ABS_SOL_ERROR);
+			  ABS_SOL_ERROR);
   ASSERT_NEAR(quadratic_function(sol_a, sol_b, sol_c, p3.x), p3.y,
-              ABS_SOL_ERROR);
+			  ABS_SOL_ERROR);
 }
 
 TEST(SolverTestInput, NullPoints) {
   point_t p1;
 
   ASSERT_EQ(find_quadratic_coefficients(NULL, &p1, &p1, NULL, NULL, NULL),
-            WRONG_INPUT);
+			WRONG_INPUT);
   ASSERT_EQ(find_quadratic_coefficients(&p1, NULL, &p1, NULL, NULL, NULL),
-            WRONG_INPUT);
+			WRONG_INPUT);
   ASSERT_EQ(find_quadratic_coefficients(&p1, &p1, NULL, NULL, NULL, NULL),
-            WRONG_INPUT);
+			WRONG_INPUT);
 }
 
 TEST(SolverTestInput, LinearityPropertiesValid) {
@@ -67,7 +67,7 @@ TEST(SolverTestInput, LinearityPropertiesValid) {
   point_t p3 = {3.0, 3.0};
 
   ASSERT_EQ(find_quadratic_coefficients(&p1, &p2, &p3, NULL, NULL, NULL),
-            SOLUTION_FOUND);
+			SOLUTION_FOUND);
 }
 
 TEST(SolverTestInput, LinearityPropertiesInvalid) {
@@ -75,9 +75,9 @@ TEST(SolverTestInput, LinearityPropertiesInvalid) {
   point_t p2 = {0.0, 0.0};
 
   ASSERT_EQ(find_quadratic_coefficients(&p1, &p1, &p2, NULL, NULL, NULL),
-            NON_INDEPENDENT_SYSTEM);
+			NON_INDEPENDENT_SYSTEM);
   ASSERT_EQ(find_quadratic_coefficients(&p1, &p2, &p1, NULL, NULL, NULL),
-			NON_INDEPENDENT_SYSTEM);
+            NON_INDEPENDENT_SYSTEM);
   ASSERT_EQ(find_quadratic_coefficients(&p2, &p1, &p1, NULL, NULL, NULL),
-			NON_INDEPENDENT_SYSTEM);
+            NON_INDEPENDENT_SYSTEM);
 }
